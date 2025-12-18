21994a62f9c6e4a4929098db56091b5335bb053c
Apurv Tiwari <apurv@andromedasurgical.com>
2025-12-17 18:44:57 -0800

    chatgpt connection

--- nvim/lua/config/chatgpt-config.lua
+++ nvim/lua/config/chatgpt-config.lua
@@ -23,6 +23,34 @@ gpt.setup({
     default_agent = "GPT52",
 })
 
+local function gp_comment_prompt()
+    local ft = vim.bo.filetype
+
+    if ft == "python" then
+        return table.concat({
+            "Add pythonic documentation:",
+            " Use Google-style docstrings (triple quotes) for functions/classes.",
+            " Add minimal inline comments only where intent is not obvious",
+            " Do not change the code.",
+        }, " ")
+    elseif ft == "cpp" or ft == "c" then
+        return table.concat({
+            " Add concise Doxygen-style comments:",
+            " Use /** */ for doc blocks and // for inline comments.",
+            " Document parameters, units, ownership, and invariants.",
+            " Do not restate obvious code.",
+        }, " ")
+    else
+        return "Add concise comments appropriate for this language and its conventions."
+    end
+end
+
+vim.keymap.set("v", "<leader>gc", function()
+    local prompt = gp_comment_prompt()
+    vim.cmd(string.format("'<,'>GpRewrite %s", prompt))
+end, { desc = "Generate documentation" })
+
 vim.keymap.set("n", "<leader>gc", function()
-  vim.cmd([[GpRewrite Add concise Doxygen-style comments. Explain parameters, units, assumptions, and safety considerations. Do not restate obvious code.]])
+    local prompt = gp_comment_prompt()
+    vim.cmd(string.format("GpRewrite %s", prompt))
 end, { desc = "Generate documentation" })
